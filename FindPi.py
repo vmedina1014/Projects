3f093766196c78e4312f1be044e83057f7e9d298
Victor A Medina <vmedina1014@gmail.com>
2016-03-02 18:57:09 -0800

    Update FindPi.py

--- FindPi.py
+++ FindPi.py
@@ -1,5 +1,5 @@
 import math
-def nDigitPI(number):
+def nDigitPI(n):
   PI=str(math.pi)
-  return PI[0:number+2]
+  return PI[0:n+2]  #print Pi up to n decimal places (n+2 to include the 3.)
 print nDigitPI(4)
